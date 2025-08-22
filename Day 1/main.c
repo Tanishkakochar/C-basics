1c9b361d33365a595c9845c88844b0a0a54cf05b
Tanishkakochar <tanishkakochar3@gmail.com>
2025-08-22 18:56:28 +0530

    Commit for day 1

--- Day 1/main.c
+++ Day 1/main.c
@@ -1,1 +1,16 @@
-yaha pe code likhna hai tanishka didi
+#include <stdio.h>
+
+int main() {
+    int a, b, sum;
+    
+    // Taking input
+    scanf("%d %d", &a, &b);
+    
+    // Performing addition
+    sum = a + b;
+    
+    // Displaying output
+    printf("Sum = %d\n", sum);
+    
+    return 0;
+}
